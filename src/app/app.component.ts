56ed6e77c2ebde0626c3d02c5228b8f5a23f4cb3
AleCueto <alecuetocontacto@gmail.com>
2022-10-07 11:16:49 +0200

    Routing and icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,9 @@ export class AppComponent {
   public appPages = [
     { title: 'Home', url: '/home', icon: 'mail' },
     // { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Persons', url: '/persons', icon: 'heart' },
-    { title: 'Tasks', url: '/tasks', icon: 'archive' },
-    { title: 'Assignments', url: '/assignments', icon: 'trash' },
+    { title: 'Persons', url: '/persons', icon: 'person' },
+    { title: 'Tasks', url: '/tasks', icon: 'newspaper' },
+    { title: 'Assignments', url: '/assignments', icon: 'id-card' },
     // { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
   public labels = [/*'Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'*/];
