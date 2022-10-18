f7837310a5710c4af6f2a791976335a2cd759e1b
AleCueto <alecuetocontacto@gmail.com>
2022-10-18 11:55:50 +0200

    task service

--- src/app/pages/tasks/tasks.module.ts
+++ src/app/pages/tasks/tasks.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,13 +8,17 @@ import { TasksPageRoutingModule } from './tasks-routing.module';
 
 import { TasksPage } from './tasks.page';
 
+import { TaskComponent } from 'src/app/components/task/task.component';
+import { TaskDetailComponent } from 'src/app/components/task-detail/task-detail.component';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    TasksPageRoutingModule
+    TasksPageRoutingModule,
+    ReactiveFormsModule
   ],
-  declarations: [TasksPage]
+  declarations: [TasksPage, TaskComponent ,TaskDetailComponent]
 })
 export class TasksPageModule {}
--- src/app/pages/tasks/tasks.page.ts
+++ src/app/pages/tasks/tasks.page.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { TasksService } from 'src/app/services/tasks.service';
+TasksService;
+import { Task } from 'src/app/models/task.model';
+import { PersonComponent } from 'src/app/components/person/person.component';
+import { Form, FormBuilder, FormGroup } from '@angular/forms';
+
+import { TaskDetailComponent } from 'src/app/components/task-detail/task-detail.component';
+
+import { AlertController, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tasks',
@@ -6,10 +15,90 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks.page.scss'],
 })
 export class TasksPage implements OnInit {
+  task: Task;
+  form:FormGroup;
 
-  constructor() { }
+  constructor(
+    private taskService: TasksService,
+    private fb:FormBuilder,
+    private modal:ModalController,
+    private alert:AlertController
+    ) {
+    this.form = this.fb.group({
+      name:'',
+      description:''
+    });
+    }
 
   ngOnInit() {
   }
 
+  async presentTaskForm(task:Task){
+    const modal = await this.modal.create({
+      component:TaskDetailComponent,
+      componentProps:{
+        task:task
+      }
+    });
+
+    modal.present();
+    modal.onDidDismiss().then(result=>{
+      if(result && result.data){
+        switch(result.data.mode){
+          case 'New':
+            this.taskService.addPerson(result.data.task);
+            break;
+          case 'Edit':
+            this.taskService.updatePerson(result.data.task);
+            break;
+          default:
+        }
+    }
+  });
+  }
+
+  getTasks(){
+    return this.taskService.getTasks();
+  }
+
+
+  onNewTask(){
+    this.presentTaskForm(null);
+  }
+
+  onEditTask(task){
+    this.presentTaskForm(task);
+  }
+
+
+  async onDeleteAlert(task){
+    const alert = await this.alert.create({
+      header: '¿Está seguro de que desear borrar a la persona?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log("Operacion cancelada");
+          },
+        },
+        {
+          text: 'Borrar',
+          role: 'confirm',
+          handler: () => {
+            this.taskService.deletePersonById(task.id);
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+  }
+
+  onDeleteTask(task){
+    this.onDeleteAlert(task);
+      
+  }
 }
