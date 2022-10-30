68eb3a6c50e848d149b02b02bd3e0e18120c2484
AleCueto <alecuetocontacto@gmail.com>
2022-10-30 14:39:24 +0100

    task acordion

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -21,9 +21,10 @@ import { AssignmentsPage } from '../pages/assignments/assignments.page';
 import { AssignmentComponent } from './components/assignment/assignment.component';
 import { AssignmentDetailComponent } from './components/assignment-detail/assignment-detail.component';
 import { PersonSelectableComponent } from './components/person-selectable/person-selectable.component';
+import { TaskSelectableComponent } from './components/task-selectable/task-selectable.component';
 
 @NgModule({
-  declarations: [TasksPage, TaskComponent ,TaskDetailComponent, PersonsPage, PersonComponent, PersonDetailComponent, AssignmentsPage, AssignmentComponent, AssignmentDetailComponent, PersonSelectableComponent],
+  declarations: [TasksPage, TaskComponent ,TaskDetailComponent, PersonsPage, PersonComponent, PersonDetailComponent, AssignmentsPage, AssignmentComponent, AssignmentDetailComponent, PersonSelectableComponent, TaskSelectableComponent],
   imports: [
     CommonModule,
     FormsModule,
